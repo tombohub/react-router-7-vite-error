cbb97aa35893ac4a9bace920674e81e03da46caf
me <tkutlesa@gmail.com>
2025-04-21 20:32:16 -0400

    this import give error

--- app/routes/koko.tsx
+++ app/routes/koko.tsx
@@ -1,4 +1,4 @@
-import { type Route } from "../.././.react-router/types/app/routes/+types/koko";
+import { type Route } from "./+types/koko";
 
 export default function Koko() {
   return <>kkokok</>;
